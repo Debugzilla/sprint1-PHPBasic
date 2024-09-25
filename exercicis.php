04f6efc98fbdba773a070d25d49fd03ec825f938
Arnau Erta <aerta7@gmail.com>
2024-09-25 14:36:03 +0200

    exercici2

--- exercicis.php
+++ exercicis.php
@@ -27,6 +27,43 @@ echo "<h1>".Nom ."</h1>"; //visualitza la constant en format titol
 
 
 
+//Exercici 2
+//Imprimeix per pantall "Hello World" utilitzant una variable. en acabat:
+
+echo "<u>Exercici 2</u>" ."<br/>";
+$V_Hola = "Hello, World!";
+
+echo $V_Hola . "<br/>";
+
+//transforma tots els caracters de l'string a majuscules i imprimeix en pantalla
+
+$V_Hola = mb_convert_case($V_Hola, MB_CASE_UPPER, "UTF-8");
+echo $V_Hola . "<br/>";
+
+//Imprimeix per pantalla la mida (longitud) de la variable.
+
+$Long = strlen($V_Hola);
+echo "Longitud del string : ". $Long . "<br/>";
+
+//Imprimeix per pantalla l'string en ordre invers de caràcters.
+$O_Invers = strrev($V_Hola);
+echo "Orden inverso del string = " .$O_Invers ."<br/>";
+
+
+//Crea una nova variable amb el contingut “Aquest és el curs de PHP” i imprimeix per pantalla la concatenació de tots dos strings.
+$Content = " Aquest és el curs de PHP";
+echo $V_Hola  . $Content  ."<br/><br/>";
+
+
+
+
+
+
+
+
+
+
+
 
 
 
