6ecd969e7b05dd22456f13e672c12aa2f6e3cc2c
Arnau Erta <aerta7@gmail.com>
2024-09-30 19:43:06 +0200

    Modificaciones ejercicios nivel1

---
+++ Basic-PHP/prova.php
@@ -0,0 +1,150 @@
+
+<html>
+<head>
+    <title>Tutorial de PHP</title>
+</head>
+<body>
+
+<?php
+
+    //echo "<h1>Hola Mundo</h1>";
+    //$nombre = "Arnau";
+
+    //echo "Mi nombre es " . $nombre; //Para concatenar cadenas se usa el punto (.)
+
+    //integer float String Boolean array Object
+    //define("PI", 3.14159);
+
+    //echo "El valor de PI es " . PI; "<br>"
+    // exercici 1 defineix una variable de cada tipus: integer,double,string,boolean
+//imprimeixles per pantalla.
+//despres crea una constant que inclogui el teu nom i mostraho en format titol per pantalla
+/*
+    echo " 6  +  4  = ". (6 + 4) . "<br/>";
+    echo " 6  -  4  = ". (6 - 4) . "<br/>";
+    echo " 6  *  4  = ". (6 * 4) . "<br/>";
+    echo " 6  /  4  = ". (6 / 4) . "<br/>";  
+    echo " 6  %  4  = ". (6 % 4) . "<br/>";
+    */
+
+   // $miNumero = 6;
+   // $refNumero = &$miNumero;
+
+    //$miNumero = 20;
+    //$resultado = ($miNumero < 40) ? "Tu numero es menor a 40" : "Tu numero no es mayor a 40";
+    
+/*$usuario = "Beto";
+
+    switch($usuario) {
+        case "Luis":
+            echo "Hola Luis";
+            break;
+        case "Henry":
+            echo "Hola Henry";
+            break;
+        case "Luisa":
+            echo "Hola Luisa";
+            break;
+
+        default:
+        echo "No puedo adivinar tu nombre";
+        break;
+
+    }
+*/
+
+/*$miNumero = 0;
+
+while($miNumero < 10) {
+    echo $miNumero;
+    $miNumero++;
+}
+
+*/
+
+
+
+/*for($num = 1; $num <= 10; $num++) {
+    echo $num;
+    if($num !== 10) {
+        echo ", ";
+    } else {
+        break;
+    }
+}
+*/
+//Devuelve los numeros del 1 al 10, con comas.
+
+/*$personas = array("Henry", "Luis", "Carmen", "Beto");
+
+//echo count($personas);
+foreach($personas as $persona) {
+    echo $persona . "<br/>";
+}
+    */
+//mostramos con el foreach cada nombre almacenado en el array
+
+//$personas = array("Luis" => 20, "Beto" => "30", "Carmen" => 25);
+
+//foreach($personas as $key => $val) {
+   // echo $key . "tiene ". $val . "<br/>";
+//   echo "$key tiene $val <br/>";
+
+//}
+
+
+
+
+//$cadena = "Los colores";
+//echo strlen($cadena);
+//echo strlen(ltrim($cadena));
+
+
+
+    //echo $resultado;
+    //echo $refNumero; //20
+
+    //$miEdad = 20;
+   /* if($miEdad > 30){
+        echo "Mi edad es mayor a 10";
+
+    } else if ($miEdad === 20) {
+        echo "Tu edad es 20";
+
+    }
+
+    else {
+        echo "No puedo adivinar tu edad";
+    }
+    */
+
+   /* if(($miEdad === 20) || ($miEdad < 25)) {
+        echo "Tu edad es 20";
+
+    }
+   */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
+
+
